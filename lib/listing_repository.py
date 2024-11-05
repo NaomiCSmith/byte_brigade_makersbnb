e011415127fd213e8d3e7024a0d7c013c7bdad06
harrman17 <harmankapoor178@gmail.com>
2024-11-05 11:31:09 +0000

    listing and listing repository classes with tests

---
+++ lib/listing_repository.py
@@ -0,0 +1,34 @@
+from lib.listing import *
+
+class ListingRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute("SELECT * FROM listings")
+
+        listings = []
+
+        for row in rows:
+            item = Listing(row['id'], row['name'], row['description'], row['price_per_night'], row['user_id'])
+            listings.append(item)
+
+        return listings
+    
+    def find(self, id):
+        rows = self._connection.execute("SELECT * FROM listings WHERE id = %s", [id])
+
+        row = rows[0]
+
+        return Listing(row['id'], row['name'], row['description'], row['price_per_night'], row['user_id'])
+
+    def create(self, listing):
+        rows = self._connection.execute("INSERT INTO listings (name, description, price_per_night, user_id) VALUES (%s, %s, %s, %s)", [listing.name, listing.description, listing.price_per_night, listing.user_id])
+
+        return None
+
+
+    def delete(self, id):
+        self._connection.execute("DELETE FROM listings WHERE id = %s", [id])
+
+        return None
