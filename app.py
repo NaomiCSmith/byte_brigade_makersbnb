6510823d2c212f95782785172ff4c63318bca181
Doug Fairfield <fairfielddouglas@gmail.com>
2024-11-05 13:08:08 +0000

    POST route added for adding new listing in app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from lib.database_connection import get_flask_database_connection
+from lib.listing_repository import ListingRepository, Listing
 
 # Create a new Flask app
 app = Flask(__name__)
@@ -18,6 +19,27 @@ def get_index():
 @app.route('/add_listing', methods=['GET'])
 def get_add_listing():
     return render_template('add_listing.html')
+
+@app.route("/listings/new", methods=['POST'])
+def post_new_listing():
+    connection = get_flask_database_connection(app)
+    repository = ListingRepository(connection)
+
+    name = request.form['name']
+    description = request.form['description']
+    price = request.form['price']
+    user_id = request.form['user_id']
+    listing = Listing(None, name, description, price, user_id)
+
+    if not listing.is_valid():
+        errors = listing.generate_errors()
+        return render_template('templates/add_listing.html', errors=errors)
+    
+    repository.create(listing)
+    return redirect(f"/index")
+
+
+
 
 # These lines start the server if you run this file directly
 # They also start the server configured to use the test database
