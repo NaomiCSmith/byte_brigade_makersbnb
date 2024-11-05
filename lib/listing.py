56e7452e0315791d1228602fbdca4fe3f98d4f3a
Doug Fairfield <fairfielddouglas@gmail.com>
2024-11-05 13:42:35 +0000

    price_per_night to price in listings.py and updaters tests for add_listing

--- lib/listing.py
+++ lib/listing.py
@@ -1,23 +1,23 @@
 class Listing:
-    def __init__(self, id, name, description, price_per_night, user_id):
+    def __init__(self, id, name, description, price, user_id):
         self.id = id
         self.name = name
         self.description = description
-        self.price_per_night = price_per_night
+        self.price = price
         self.user_id = user_id
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"Listing({self.id}, {self.name}, {self.description}, {self.price_per_night}, {self.user_id})"
+        return f"Listing({self.id}, {self.name}, {self.description}, {self.price}, {self.user_id})"
     
     def is_valid(self):
         if self.name is None or self.name == "":
             return False
         if self.description is None or self.description == "":
             return False
-        if self.price_per_night is None or not isinstance(self.price_per_night, int):
+        if self.price is None or not isinstance(self.price, int):
             return False
         return True
     def generate_errors(self):
@@ -26,7 +26,7 @@ class Listing:
             errors.apppend("")
         if self.description is None or self.description == "":
             errors.apppend("")
-        if self.price_per_night is None or not isinstance(self.price_per_night, int):
+        if self.price is None or not isinstance(self.price, int):
             errors.apppend("")
         if len(errors) == 0:
             return None
