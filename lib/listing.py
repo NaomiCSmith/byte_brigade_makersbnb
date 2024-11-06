44f300da5ba8bf4dadede81caf751fdee2cfbe84
harrman17 <harmankapoor178@gmail.com>
2024-11-06 10:16:27 +0000

    request booking routes and templates with tests

--- lib/listing.py
+++ lib/listing.py
@@ -1,13 +1,14 @@
 class Listing:
-    def __init__(self, id, name, description, price_per_night, user_id):
+    def __init__(self, id, name, description, price, user_id):
         self.id = id
         self.name = name
         self.description = description
-        self.price_per_night = price_per_night
+        self.price = price
         self.user_id = user_id
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"Listing({self.id}, {self.name}, {self.description}, {self.price_per_night}, {self.user_id})"
+        return f"Listing({self.id}, {self.name}, {self.description}, {self.price}, {self.user_id})"
+    
