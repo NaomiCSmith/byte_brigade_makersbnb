f3385e1f2ff3640ce9dadad59ce0dddb2480a380
Aaron Bridger <aaronjbridger@gmail.com>
2024-11-07 12:01:48 +0000

    lib/listing.py error

--- lib/listing.py
+++ lib/listing.py
@@ -10,5 +10,25 @@ class Listing:
         return self.__dict__ == other.__dict__
     
     def __repr__(self):
-        return f"Listing({self.id}, {self.name}, {self.description}, {self.price}, {self.user_id})"
+        return f"Listing({self.id}, {self.name}, {self.description}, {self.price_per_night}, {self.user_id})"
     
+    def is_valid(self):
+        if self.name is None or self.name == "":
+            return False
+        if self.description is None or self.description == "":
+            return False
+        if self.price_per_night is None or not isinstance(self.price_per_night, int):
+            return False
+        return True
+    def generate_errors(self):
+        errors = []
+        if self.name is None or self.name == "":
+            errors.apppend("")
+        if self.description is None or self.description == "":
+            errors.apppend("")
+        if self.price_per_night is None or not isinstance(self.price_per_night, int):
+            errors.apppend("")
+        if len(errors) == 0:
+            return None
+        else:
+            return ", ".join(errors)
